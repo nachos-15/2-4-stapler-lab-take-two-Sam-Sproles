934371e69da3324b7d3c874367fb437a192f73c5
nachos-15 <74326908+nachos-15@users.noreply.github.com>
2024-08-23 13:58:18 -0500

    Stapler.java
    2nd Attempt at fixing Stapler.java
    
    Note: The tester seems to think that I'm not ignoring the negative staples, but this should work. I tried several variants of this and none got that to work. Assuming I'm interpreting "ignore" correctly.

--- Stapler.java
+++ Stapler.java
@@ -4,12 +4,13 @@ class Stapler{
     //Data
     private int staples;
     private boolean isOpen;
-		private int capacity;
+	private int capacity;
     
     //Methods (Behaviors)
     public Stapler(){
         staples = 0;
         isOpen = false;
+		capacity = 500;
     }
     
     public void loadStaples(int s){
@@ -17,9 +18,11 @@ class Stapler{
 		//If s is not positive, ignore the value
         if(staples < 0){
 			staples = s;
+			// The tester seems to think that I'm not ignoring the negative staples, but this should work. I tried
+			// several variants of this and none got that to work. Assuming I'm interpreting "ignore" correctly. 
 		}
-		else if((staples + s) >= 500){
-			staples = 500;
+		else if((staples + s) >= capacity){
+			staples = capacity;
 		}
 		else{
 			staples += s;
@@ -31,9 +34,10 @@ class Stapler{
         //1.  If the stapler is closed and not out of staples, staple!
         //2.  Reduce the number of staples by 1 & return true.
         //If there was a problem stapling (not closed, out of staples) return false;
-		if(isOpen && staples > 0){
+		if(!isOpen && staples > 0){
 			staples -= 1;
 			return true;
+		}
         else{
     	    return false;
 		}
@@ -59,7 +63,7 @@ class Stapler{
 		/** Method to get print information about the Stapler
 	@return returns a phrase like "This stapler has room for x staples" where x is the remaining capacity after accounting for staples in the Stapler
 	**/
-		public String toString(){
-			return "This stapler has room for " + (500 - staples) + "staples.";
-		}
+	public String toString(){
+		return "This stapler has room for " + (capacity - staples) + "staples.";
+	}
 }
